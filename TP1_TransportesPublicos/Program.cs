edbd4b6f0d4d7f620b241740335f8a3950064c8c
emilioTalarico <emilio.talarico@hotmail.com>
2022-05-29 19:44:05 -0300

    Se agregó metodos estáticos para una mejor visualización del código.

--- TP1_TransportesPublicos/Program.cs
+++ TP1_TransportesPublicos/Program.cs
@@ -8,6 +8,7 @@ namespace TP1_TransportesPublicos
 {
     internal class Program
     {
+             
         static void Main(string[] args)
         {
             //Color de fondo y letras de la consola.
@@ -19,6 +20,7 @@ namespace TP1_TransportesPublicos
             List<Taxi> lTaxi = new List<Taxi>();
 
             ConsoleKeyInfo opcion;
+
 
             //Empieza el menú
             do
@@ -38,119 +40,152 @@ namespace TP1_TransportesPublicos
                 switch (opcion.KeyChar)
                 {
                     case '1':
-                        Console.Clear();
-                        Console.WriteLine(":::::MENU AGREGAR TRANSPORTE:::::");     
-                        Console.WriteLine("\nTipo de transporte: ");
-                        Console.WriteLine("\n1 - Omnibus");
-                        Console.WriteLine("2 - Taxi");
-                        Console.WriteLine("ESC - Salir");
-                        Console.Write("Ingrese una opción: ");
-                        do
-                        {
-                            opcion = Console.ReadKey(true);
-                        } while (((int)opcion.KeyChar != 27) && (opcion.KeyChar < '1' || opcion.KeyChar > '2'));
-
-                        switch (opcion.KeyChar)
-                        {
-                            case '1':
-                                Console.Clear();
-                                try
-                                {
-                                    Console.WriteLine(":::::MENU NUEVO OMNIBUS:::::");
-                                    Console.WriteLine("\nIngrese los datos para el nuevo Omnibus:");
-                                    Console.Write("Marca: ");
-                                    string marca = Console.ReadLine();
-                                    Console.Write("Motor: ");
-                                    string motor = Console.ReadLine();
-                                    Console.Write("Capacidad de pasajeros: ");
-                                    int pasajeros = Convert.ToInt32(Console.ReadLine());
-
-
-                                    Omnibus omnibus = new Omnibus(marca, motor, pasajeros);
-                                    lOmnibus.Add(omnibus);
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Omnibus agregado con éxito..");
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Presione una tecla para continuar..");
-                                    Console.ReadKey(true);
-                                }
-                                catch (Exception ex)
-                                {
-
-                                    Console.WriteLine("Uy, apareció el siguiente error: " + ex.Message);
-
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Presione una tecla para continuar..");
-                                    Console.ReadKey();
-                                }
-                                Console.WriteLine("\n");
-                                break;
-
-                            case '2':
-                                Console.Clear();
-                                try
-                                {
-                                    Console.WriteLine(":::::MENU NUEVO TAXI:::::");
-                                    Console.WriteLine("\nIngrese los datos para el nuevo Taxi:");
-                                    Console.Write("Marca: ");
-                                    string marca = Console.ReadLine();
-                                    Console.Write("Motor: ");
-                                    string motor = Console.ReadLine();
-                                    Console.Write("Capacidad de pasajeros: ");
-                                    int pasajeros = Convert.ToInt32(Console.ReadLine());
-
-
-                                    Taxi taxi = new Taxi(marca, motor, pasajeros);
-                                    lTaxi.Add(taxi);
-
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Taxi agregado con éxito..");
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Presione una tecla para continuar..");
-                                    Console.ReadKey(true);
-                                }
-                                catch (Exception ex)
-                                {
-
-                                    Console.WriteLine("Uy, apareció el siguiente error: " + ex.Message);
-                                    
-                                    Console.WriteLine("");
-                                    Console.WriteLine("Presione una tecla para continuar..");
-                                    Console.ReadKey();
-                                }
-                                Console.WriteLine("\n");
-                                break;
-                        }
+                        NuevoTransporte(lOmnibus,lTaxi);
                         break;
 
                     case '2':
-
-                        Console.Clear();
-                        Console.WriteLine(":::::MENU LISTAR TRANSPORTES:::::");
-                        Console.WriteLine("");
-                        Console.WriteLine(":::...Omnibus...:::");
-                        Console.WriteLine("");
-                        foreach (var item in lOmnibus)
-                        {
-                            Console.WriteLine(item);
-                        }
-
-                        Console.WriteLine(":::...Taxi...:::");
-                        Console.WriteLine("");
-                        foreach (var item in lTaxi)
-                        {
-                            Console.WriteLine(item);
-                        }
-
-                        Console.WriteLine("Presione una tecla para continuar...");
-                        Console.ReadKey();
-
+                        ListarTransportes(lOmnibus,lTaxi);
                         break;
                 }
 
 
             } while ((int)opcion.KeyChar != 27);
 
+        }
+
+        public static void ListarTransportes(List<Omnibus> lOmnibus, List<Taxi> lTaxi)
+        {
+            Console.Clear();
+            Console.WriteLine(":::::MENU LISTAR TRANSPORTES:::::");
+            Console.WriteLine("");
+            Console.WriteLine(":::...Omnibus...:::");
+            Console.WriteLine("");
+            foreach (var item in lOmnibus)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(":::...Taxi...:::");
+            Console.WriteLine("");
+            foreach (var item in lTaxi)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+
+        }
+        public static void NuevoTransporte(List<Omnibus> lOmnibus, List<Taxi> lTaxi)
+        {
+            ConsoleKeyInfo opcion;
+
+            Console.Clear();
+            Console.WriteLine(":::::MENU AGREGAR TRANSPORTE:::::");
+            Console.WriteLine("\nTipo de transporte: ");
+            Console.WriteLine("\n1 - Omnibus");
+            Console.WriteLine("2 - Taxi");
+            Console.WriteLine("ESC - Salir");
+            Console.Write("Ingrese una opción: ");
+            do
+            {
+                opcion = Console.ReadKey(true);
+            } while (((int)opcion.KeyChar != 27) && (opcion.KeyChar < '1' || opcion.KeyChar > '2'));
+
+            switch (opcion.KeyChar)
+            {
+                case '1':
+                    NuevoOmnibus(lOmnibus);
+                    break;
+
+                case '2':
+                    NuevoTaxi(lTaxi);
+                    break;
+            }
+        }
+        public static void NuevoTaxi(List<Taxi> lTaxi)
+        {
+            Console.Clear();
+            try
+            {
+                Console.WriteLine(":::::MENU NUEVO TAXI:::::");
+                Console.WriteLine("\nIngrese los datos para el nuevo Taxi:");
+                Console.Write("Marca: ");
+                string marca = Console.ReadLine();
+                Console.Write("Motor: ");
+                string motor = Console.ReadLine();
+                Console.Write("Capacidad de pasajeros, debe ser entre 1 y 5: ");
+                int pasajeros = Convert.ToInt32(Console.ReadLine());
+                if (pasajeros < 1 || pasajeros>5)
+                {
+                    Console.WriteLine("Ingresó un numero fuera del rango permitido");
+                    Console.WriteLine("Toque una tecla para continuar...");
+                    Console.ReadKey(true);
+                    return;
+                }
+
+
+                Taxi taxi = new Taxi(marca, motor, pasajeros);
+                lTaxi.Add(taxi);
+
+                Console.WriteLine("");
+                Console.WriteLine("Taxi agregado con éxito..");
+                Console.WriteLine("");
+                Console.WriteLine("Presione una tecla para continuar..");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Uy, apareció el siguiente error: " + ex.Message);
+
+                Console.WriteLine("");
+                Console.WriteLine("Presione una tecla para continuar..");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n");
+        }
+        public static void NuevoOmnibus(List<Omnibus> lOmnibus)
+        {
+            Console.Clear();
+            try
+            {
+                Console.WriteLine(":::::MENU NUEVO OMNIBUS:::::");
+                Console.WriteLine("\nIngrese los datos para el nuevo Omnibus:");
+                Console.Write("Marca: ");
+                string marca = Console.ReadLine();
+                Console.Write("Motor: ");
+                string motor = Console.ReadLine();
+                Console.Write("Capacidad de pasajeros, debe ser entre 1 y 100: ");
+                int pasajeros = Convert.ToInt32(Console.ReadLine());
+                if (pasajeros < 1 || pasajeros > 100)
+                {
+                    Console.WriteLine("Ingresó un numero fuera del rango permitido");
+                    Console.WriteLine("Toque una tecla para continuar...");
+                    Console.ReadKey(true);
+                    return;
+                }
+
+
+                Omnibus omnibus = new Omnibus(marca, motor, pasajeros);
+                lOmnibus.Add(omnibus);
+                Console.WriteLine("");
+                Console.WriteLine("Omnibus agregado con éxito..");
+                Console.WriteLine("");
+                Console.WriteLine("Presione una tecla para continuar..");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Uy, apareció el siguiente error: " + ex.Message);
+
+                Console.WriteLine("");
+                Console.WriteLine("Presione una tecla para continuar..");
+                Console.ReadKey();
+            }
+            Console.WriteLine("\n");
+            
         }
     }
 }
