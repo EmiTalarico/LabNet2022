cdf9dbec3b99d0649305847aa3c813f8b1b39036
emilioTalarico <emilio.talarico@hotmail.com>
2022-06-07 10:55:45 -0300

    COMMIT INICIAL. Se separó en capas, se agregó la conexión y probó el correcto funcionamiento.

---
+++ TP4.EntityFrameWork/TP4.EntityFrameWork.Logic/ProductsLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.EntityFrameWork.Data;
+using TP4.EntityFrameWork.Entities;
+
+namespace TP4.EntityFrameWork.Logic
+{
+    public class ProductsLogic
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductsLogic()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public List<Products> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
---
+++ TP4.EntityFrameWork/TP4.EntityFrameWork.UI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.EntityFrameWork.Logic;
+
+namespace TP4.EntityFrameWork.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductsLogic productslogic = new ProductsLogic();
+
+
+            foreach (var item in productslogic.GetAll())
+            {
+                Console.WriteLine($"{item.ProductName} - {item.UnitPrice}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
