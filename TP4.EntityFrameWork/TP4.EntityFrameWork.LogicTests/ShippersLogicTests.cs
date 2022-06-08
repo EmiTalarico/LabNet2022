982b350381c36e4c8f37ec2e46df8248fe85f952
emilioTalarico <emilio.talarico@hotmail.com>
2022-06-08 03:19:34 -0300

    Se agregó 1 UnitTest. Metodo Add de Shippers.

---
+++ TP4.EntityFrameWork/TP4.EntityFrameWork.LogicTests/ShippersLogicTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP4.EntityFrameWork.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP4.EntityFrameWork.Data;
+using TP4.EntityFrameWork.Entities;
+
+namespace TP4.EntityFrameWork.Logic.Tests
+{
+    [TestClass()]
+    public class ShippersLogicTests
+    {
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            // arrange
+            ShippersLogic shippersLogic = new ShippersLogic();  //Instancio
+            Shippers shipperAdd = new Shippers();
+            shipperAdd.CompanyName = "UnitTest";  //Agrego campos
+            shipperAdd.Phone = "UnitTest";
+
+            // act
+            shippersLogic.Add(shipperAdd);  //a utilizar como resultado esperado
+
+            // assert
+            List<Shippers> newList = shippersLogic.GetAll().ToList();  //obtengo lista actualizada
+            Shippers checkList = newList[newList.Count() - 1];  //chequeo con la lista actualizada
+            Assert.AreEqual(shipperAdd, checkList);  //compruebo
+        }
+    }
+}
